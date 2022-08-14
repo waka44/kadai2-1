e6bfe41cca854803cd83f3d7115ad27f204e28ce
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-08-14 10:19:22 +0000

    kadai2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,34 @@
+class BooksController < ApplicationController
+   
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
